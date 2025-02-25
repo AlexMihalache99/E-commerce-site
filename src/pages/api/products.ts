4a7655914f521d712a7761cc9f242b2a9afd7c95
Alexandru Mihalache <alexandrumihalache99@gmail.com>
2025-02-25 20:55:59 +0200

    fixed categories not displaying

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-
-const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     try {
-      const products = await prisma.product.findMany();
+      const products = await prisma.product.findMany({
+        include: { category: true },
+      });
       res.status(200).json(products);
     } catch (error) {
       res.status(500).json({ error: "Failed to fetch products" });
